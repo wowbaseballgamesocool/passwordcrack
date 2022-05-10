014c164256dbc09d95cc4f580ff7c7dc792cacd8
wowbaseballgamesocool <adambishop555@gmail.com>
2022-05-10 14:26:09 -0400

    +10-1 method

--- crack.py
+++ crack.py
@@ -58,3 +58,24 @@ for i in range(0, trys_per_method + 1):
     
 end = time.time()
 print("even/odd method took " + str(round(end - start, 3)) + "s and " + str(round(mean(list), 2)) + " attempts")
+
+
+
+# +10-1 method
+# this method goes up by 10 each time, then 9, then 8 and so on
+start = time.time()
+list = []
+for i in range(0, trys_per_method + 1):
+    password = random.randint(1, 10000)
+    x = 0
+    mode = 0
+    attempts = 0
+    while x != password:
+        x += 10 - mode
+        attempts += 1
+        if x > 10000: mode += 1; x = 0
+    list.append(attempts)
+    #print(attempts)
+    
+end = time.time()
+print("+10-1 method took " + str(round(end - start, 3)) + "s and " + str(round(mean(list), 2)) + " attempts")
