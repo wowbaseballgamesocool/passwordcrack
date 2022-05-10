60d8ad8e6a41283b0e5b1217aac71300bf70ef29
wowbaseballgamesocool <adambishop555@gmail.com>
2022-05-10 18:42:42 -0400

    moved all methods into functions

--- crack.py
+++ crack.py
@@ -2,80 +2,86 @@ import random, time
 from statistics import mean
 
 trys_per_method = 10000
+possible_passwords = 10000
+# default is 10000 for both
 
 
 # +1 method
-
-start = time.time()
-list = []
-for i in range(0, trys_per_method + 1):
-    password = random.randint(1, 10000)
-    x = 1
-    attempts = 0
-    while x != password:
-        x += 1
-        attempts += 1
-    list.append(attempts)
-    #print(attempts)
-    
-end = time.time()
-print("+1 method took " + str(round(end - start, 3)) + "s and " + str(round(mean(list), 2)) + " attempts")
+def plus1method():
+    start = time.time()
+    list = []
+    for i in range(0, trys_per_method + 1):
+        password = random.randint(1, possible_passwords)
+        x = 1
+        attempts = 0
+        while x != password:
+            x += 1
+            attempts += 1
+        list.append(attempts)
+        #print(attempts)
+        
+    end = time.time()
+    print("+1 method took " + str(round(end - start, 3)) + "s and " + str(round(mean(list), 2)) + " attempts")
 
 
 
 # -1 method
-start = time.time()
-list = []
-for i in range(0, trys_per_method + 1):
-    password = random.randint(1, 10000)
-    x = 10000
-    attempts = 0
-    while x != password:
-        x -= 1
-        attempts += 1
-    list.append(attempts)
-    #print(attempts)
-    
-end = time.time()
-print("-1 method took " + str(round(end - start, 3)) + "s and " + str(round(mean(list), 2)) + " attempts")
+def minus1method():
+    start = time.time()
+    list = []
+    for i in range(0, trys_per_method + 1):
+        password = random.randint(1, possible_passwords)
+        x = possible_passwords
+        attempts = 0
+        while x != password:
+            x -= 1
+            attempts += 1
+        list.append(attempts)
+        #print(attempts)
+        
+    end = time.time()
+    print("-1 method took " + str(round(end - start, 3)) + "s and " + str(round(mean(list), 2)) + " attempts")
 
 
 
 
 # even/odd method
-start = time.time()
-list = []
-for i in range(0, trys_per_method + 1):
-    password = random.randint(1, 10000)
-    x = 0
-    attempts = 0
-    while x != password:
-        x += 2
-        attempts += 1
-        if x > 10000: x = 1
-    list.append(attempts)
-    #print(attempts)
-    
-end = time.time()
-print("even/odd method took " + str(round(end - start, 3)) + "s and " + str(round(mean(list), 2)) + " attempts")
+def evenoddmethod()
+    start = time.time()
+    list = []
+    for i in range(0, trys_per_method + 1):
+        password = random.randint(1, possible_passwords)
+        x = 0
+        attempts = 0
+        while x != password:
+            x += 2
+            attempts += 1
+            if x > possible_passwords: x = 1
+        list.append(attempts)
+        #print(attempts)
+        
+    end = time.time()
+    print("even/odd method took " + str(round(end - start, 3)) + "s and " + str(round(mean(list), 2)) + " attempts")
 
 
 
 # +10-1 method
 # this method goes up by 10 each time, then 9, then 8 and so on
-start = time.time()
-list = []
-for i in range(0, trys_per_method + 1):
-    password = random.randint(1, 10000)
-    x = 0
-    mode = 0
-    attempts = 0
-    while x != password:
-        x += 10 - mode
-        attempts += 1
-        if x > 10000: mode += 1; x = 0
-    list.append(attempts)
-    #print(attempts)
-    
-end = time.time()
-print("+10-1 method took " + str(round(end - start, 3)) + "s and " + str(round(mean(list), 2)) + " attempts")
+def plus10minus1method():
+    start = time.time()
+    list = []
+    for i in range(0, trys_per_method + 1):
+        password = random.randint(1, possible_passwords)
+        x = 0
+        mode = 0
+        attempts = 0
+        while x != password:
+            x += 10 - mode
+            attempts += 1
+            if x > possible_passwords: mode += 1; x = 0
+        list.append(attempts)
+        #print(attempts)
+        
+    end = time.time()
+    print("+10-1 method took " + str(round(end - start, 3)) + "s and " + str(round(mean(list), 2)) + " attempts")
+
