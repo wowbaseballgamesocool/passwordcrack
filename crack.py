65afa36200c59d504dda883cbcfb10de4eca3677
wowbaseballgamesocool <adambishop555@gmail.com>
2022-05-10 22:46:46 -0400

    support for terminals

--- crack.py
+++ crack.py
@@ -1,28 +1,15 @@
-from ast import Try
-import random, time
+import random, time, sys
 from statistics import mean
 
 trys_per_method = 10000
 possible_passwords = 10000
 # default is 10000 for both
+
+try: trys_per_method = int(sys.argv[2]); possible_passwords = int(sys.argv[1]) # for use in terminal [ > crack.py 10000 10000 ]
+except: pass
+
 print("This will calculate which method is the fastest to brute force a password with " + str(possible_passwords) + " combinations, " + str(trys_per_method) + " times")
 
-# +1 method
-def plus1method():
-    start = time.time()
-    list = []
-    for i in range(0, trys_per_method + 1):
-        password = random.randint(1, possible_passwords)
-        x = 1
-        attempts = 0
-        while x != password:
-            x += 1
-            attempts += 1
-        list.append(attempts)
-        #print(attempts)
-        
-    end = time.time()
-    print("+1 method took " + str(round(end - start, 3)) + "s and " + str(round(mean(list), 2)) + " attempts")
 
 
 
