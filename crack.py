044fdb9ae978740022238b03a078db6128f563aa
wowbaseballgamesocool <adambishop555@gmail.com>
2022-05-11 23:22:42 -0400

    small optimization for counting attempts

--- crack.py
+++ crack.py
@@ -1,15 +1,34 @@
 import random, time, sys
-from statistics import mean
 
 trys_per_method = 10000
 possible_passwords = 10000
 # default is 10000 for both
 
-try: trys_per_method = int(sys.argv[2]); possible_passwords = int(sys.argv[1]) # for use in terminal [ > crack.py 10000 10000 ]
+seconds_decimal = 3
+attempts_decimal = 2
+
+
+try: trys_per_method = int(sys.argv[2]); possible_passwords = int(sys.argv[1]); seconds_decimal = int(sys.argv[3]); attempts_decimal = int(sys.argv[4])# + 1 # for use in terminal [ > crack.py 10000 10000 3 2 ]
 except: pass
 
 print("This will calculate which method is the fastest to brute force a password with " + str(possible_passwords) + " combinations, " + str(trys_per_method) + " times")
 
+# +1 method
+def plus1method():
+    start = time.time()
+    list = []
+    attempts = 1
+    for i in range(0, trys_per_method + 1):
+        password = random.randint(1, possible_passwords)
+        x = 1
+        
+        while x != password:
+            x += 1
+            attempts += 1
+        
+        
+    end = time.time()
+    print("+1 method took " + str(round(end - start, seconds_decimal)) + "s and " + str(round(attempts / trys_per_method, attempts_decimal)) + " attempts")
 
 
 
@@ -17,18 +36,18 @@ print("This will calculate which method is the fastest to brute force a password
 def minus1method():
     start = time.time()
     list = []
+    attempts = 1
     for i in range(0, trys_per_method + 1):
         password = random.randint(1, possible_passwords)
         x = possible_passwords
-        attempts = 0
+        
         while x != password:
             x -= 1
             attempts += 1
-        list.append(attempts)
-        #print(attempts)
+        
         
     end = time.time()
-    print("-1 method took " + str(round(end - start, 3)) + "s and " + str(round(mean(list), 2)) + " attempts")
+    print("-1 method took " + str(round(end - start, seconds_decimal)) + "s and " + str(round(attempts / trys_per_method, attempts_decimal)) + " attempts")
 
 
 
@@ -37,19 +56,19 @@ def minus1method():
 def evenoddmethod():
     start = time.time()
     list = []
+    attempts = 1
     for i in range(0, trys_per_method + 1):
-        password = random.randint(1, possible_passwords)
+        password = random.randint(0, possible_passwords - 1)
         x = 0
-        attempts = 0
+        
         while x != password:
             x += 2
             attempts += 1
             if x > possible_passwords: x = 1
-        list.append(attempts)
-        #print(attempts)
+
         
     end = time.time()
-    print("even/odd method took " + str(round(end - start, 3)) + "s and " + str(round(mean(list), 2)) + " attempts")
+    print("even/odd method took " + str(round(end - start, seconds_decimal)) + "s and " + str(round(attempts / trys_per_method, attempts_decimal)) + " attempts")
 
 
 
@@ -58,20 +77,20 @@ def evenoddmethod():
 def plus10minus1method():
     start = time.time()
     list = []
+    attempts = 1
     for i in range(0, trys_per_method + 1):
-        password = random.randint(1, possible_passwords)
+        password = random.randint(0, possible_passwords - 1)
         x = 0
         mode = 0
-        attempts = 0
+        
         while x != password:
             x += 10 - mode
             attempts += 1
             if x > possible_passwords: mode += 1; x = 0
-        list.append(attempts)
-        #print(attempts)
+
         
     end = time.time()
-    print("+10-1 method took " + str(round(end - start, 3)) + "s and " + str(round(mean(list), 2)) + " attempts")
+    print("+10-1 method took " + str(round(end - start, seconds_decimal)) + "s and " + str(round(attempts / trys_per_method, attempts_decimal)) + " attempts")
 
 
 plus1method()
